7f963f451ce42473d05505c0048e909c0a3b299d
Fitsum Sileshi <fitsesile@gmail.com>
2021-01-12 02:49:09 +0300

    added graphs

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,54 +1,119 @@
 import React,{useState,useEffect} from 'react'
 import { Line } from 'react-chartjs-2'
+import numeral from "numeral"
+
+
+const options={
+        legend:{
+            display: false,
+        },
+        elements:{
+            point:{
+                radius: 0,
+            },
+        },
+        maintainAspectRatio:false,
+        tooltips:{
+            mode:"index",
+            intersect:false,
+            callbacks:{
+                label: function(tooltipItem,data){
+                    return numeral(tooltipItem.value).format("+0,0");
+                },
+            },
+        },
+        scales:{
+            xAxes:[
+                {
+                    type:"time",
+                    time:{
+                        format:"MM/DD/YY",
+                        tooltipFormat:"ll",
+                    },
+                },
+            ],
+
+            yAxes:[
+                {
+                    gridlines:{
+                        display:false,
+                    },
+                    ticks:{
+                        //include a dolar sign in the ticks
+                        callback: function(value,index,values){
+                            return numeral(value).format("0a");
+                        },
+                    },
+                },
+            ],
+
+
+        },
+    };
+
+
+
 
 function LineGraph() {
     const [data,setData]=useState({})
 
+
+    const buildChartData=(data,casesType='cases')=>{
+        const chartData=[];
+        let lastDataPoint;
+    
+        for(let date in data.cases) {
+        
+            if(lastDataPoint){
+                const newDataPoint={
+                    x:date,
+                    y: data[casesType][date]-lastDataPoint
+                }
+                chartData.push(newDataPoint)
+            }
+            lastDataPoint=data[casesType][date];
+        };
+        return chartData;
+    }
 //https://disease.sh/v3/covid-19/historical/all?lastdays=120
     useEffect(()=>{
-        fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
-        .then(response=>response.json())
+        const fetchData=async()=>{
+
+    await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
+        .then((response)=>
+        {
+            return response.json()
+        })
         .then(data=>{
 
-console.log(data)
-
+            let chartData=buildChartData(data,"cases")
+            setData(chartData)
 
         })
-    },[]);
+        
+        };
 
-
-const buildChartData=data=>{
-    const chartData=[];
-    let lastDataPoint;
-
-    data.cases.forEach(date => {
+        fetchData();
     
-        if(lastDataPoint){
-            const newDataPoint={
-                x:date,
-                y: data['cases'][date]-lastDataPoint
-            }
-            chartData.push(newDataPoint)
-        }
-        lastDataPoint=data['cases'][date];
-    });
-    return chartData;
-}
-
-
-
-
-
-
+    },[]);
 
 
     return (
         <div>
-            {/* <Line>
-           
-                data
-                 options
-        </Line>  */}
+            { data?.length>0 &&(
+            <Line
+            options={options}
+                data={{
+                    datasets:[
+                        {
+                            data:data,
+                            backgroundColor: "rgba(204,16,52,0.5)",
+                            borderColor: "#CC1034"    
+                        },
+                        ],
+                }}
+        />
+            )};
         <h1>I'm a graph</h1>
         </div>
     )
