71e5ee3819b4be5a31ab960b728f9f9eaee72d99
Fitsum Sileshi <fitsesile@gmail.com>
2021-01-11 19:57:19 +0300

    material ui package added and header component started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 
 import './App.css';
+import {
+  MenuItem,
+  FormControl,
+  Select
+} from "@material-ui/core"
+
+
 
 function App() {
   return (
     <div className="app">
-       <h1>covid-19 tracker</h1>
-       
+       <h1>COVID-19 TRACKER</h1>
+       <div className="app__header">
+
+       </div>
+      <FormControl className="app__dropdown">
+          <Select
+          variant="outlined"
+          value="abc"
+          >
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">bla</MenuItem>
+          <MenuItem value="worldwide">ab</MenuItem>
+          <MenuItem value="worldwide">abl</MenuItem>
+          </Select>
+      </FormControl>
       {/** Header */}
       {/** Title + Select input dropdown field */}
 
